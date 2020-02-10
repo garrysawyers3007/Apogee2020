58a0a41be42f5fe9b85907d2b6509ea48fb7555d
anubhavajmera <anubhavajmera123@gmail.com>
2020-02-10 18:54:37 +0530

    more

--- app/src/main/java/com/bitspilani/apogeear/Adapters/MoreAdapter.java
+++ app/src/main/java/com/bitspilani/apogeear/Adapters/MoreAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -30,6 +31,12 @@ public class MoreAdapter extends RecyclerView.Adapter<MoreAdapter.MoreViewHolder
     public MoreViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.more_card_view,null);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,"clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
         return new MoreViewHolder(view);
     }
 
